38bbf0e8759a9dee7752c99955a709f1a0b1de19
ly49nkallo <65131672+ly49nkallo@users.noreply.github.com>
2021-03-02 16:45:47 -0800

    Install flask_session

---
+++ controller.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
