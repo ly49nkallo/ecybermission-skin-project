ca1962aeb0d1fda8c47b53b4bc0e071820f639a7
ly49nkallo <tyabrennan@gmail.com>
2021-03-05 14:05:37 -0800

    login functionality

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
-# sqlite3
-conn = sqlite3.connect('users.db')
-cur = conn.cursor()
+from header import execute, login_required
+
+
 
 # Boiler plate stuff
 app = Flask(__name__)
@@ -20,6 +20,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+
 # Ensure responses aren't cached
 @app.after_request
 def after_request(response):
@@ -30,9 +31,51 @@ def after_request(response):
 
 @app.route("/")
 def index():
-
+    execute("INSERT INTO users VALUES (?, ?, ?, ?")
     return render_template("index.html")
 
-def execute(command):
-    cur.execute(command)
-    con.commit()
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
