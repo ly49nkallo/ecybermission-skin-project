9ba6b33ba4dbd1604857bdf1199109d05306b9a6
ly49nkallo <tyabrennan@gmail.com>
2021-03-05 13:11:11 -0800

    add support for sqlite3

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import os
-
+import sqlite3
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
+
+# sqlite3
+conn = sqlite3.connect('users.db')
+cur = conn.cursor()
 
 # Boiler plate stuff
 app = Flask(__name__)
@@ -26,4 +30,9 @@ def after_request(response):
 
 @app.route("/")
 def index():
+
     return render_template("index.html")
+
+def execute(command):
+    cur.execute(command)
+    con.commit()
