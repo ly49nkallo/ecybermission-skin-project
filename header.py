d11c8a49b10d4f92c5fd4c52a678aea0db4b4e39
ly49nkallo <65131672+ly49nkallo@users.noreply.github.com>
2021-03-07 22:02:12 -0800

    Update header.py
    https://stackoverflow.com/questions/12309269/how-do-i-write-json-data-to-a-file

--- header.py
+++ header.py
@@ -67,3 +67,5 @@ def lookup(lat, lon, exclude=None, lang="en", units="imperial"):
 with open('data.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=4)"""
 
+#  https://stackoverflow.com/questions/12309269/how-do-i-write-json-data-to-a-file
+
